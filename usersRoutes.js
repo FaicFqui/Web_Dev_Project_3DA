7bec385362d10fb423a15c0ca167d79437e62950
FQUIHI Faiçal <134364426+FaicFqui@users.noreply.github.com>
2025-01-12 16:15:34 +0100

    ajout routeur sur une nouvelle branche router

---
+++ usersRoutes.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+//const usersController = require('./usersControllers');
+
+const users = [
+	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+]
+
+// Routes pour les utilisateurs
+// Lire tous les utilisateurs
+router.get('/', (req, res) => {
+	res.json(users)}); 
+
+// Lire un utilisateur spécifique
+router.get('/:id', (req, res) => {
+	const id = parseInt(req.params.id)
+
+	// trouve son index, verifier si le userIndex est positive
+	const userIndex = users.findIndex((user) => user.id === id)
+
+	// utilisateur non trouvé
+	if (userIndex < 0)
+		return res.status(404).json({ msg: "utilisateur non trouvé" })
+    // si el est trouvé
+	res.json(users[userIndex])
+}); 
+
+// Créer un nouvel utilisateur
+router.post('/', (req, res) => {
+	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
+	const { firstName, lastName } = req.body
+
+    if (!firstName || !lastName) {
+        return res.status(400).json({ msg: "Les champs 'firstName' et 'lastName' sont requis" });
+      }
+
+	// récupérer l'ID du dernier utilisateur en fonction du nombre d'utilisateurs dans notre variable de tableau 'users'.
+	const lastId = users[users.length - 1].id
+	// ajouter un pour créer un utilisateur unique
+	const newId = lastId + 1
+
+	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
+	const newUser = {
+		firstName,
+		lastName,
+		id: newId,
+	}
+
+	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
+	users.push(newUser)
+	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
+	res.status(201).json(newUser)}); // Créer un nouvel utilisateur
+
+// Mise à jour complète d'un utilisateur
+router.put('/:id', (req, res) => {
+    const { firstName, lastName } = req.body
+    const id = parseInt(req.params.id)
+    // trouve son index, verifier si le userIndex est positive
+	const userIndex = users.findIndex((user) => user.id === id)
+
+    if (userIndex < 0)
+		return res.status(404).json({ msg: "utilisateur non trouvé" })
+    if (firstName) users[userIndex].firstName = firstName
+	if (lastName) users[userIndex].lastName = lastName
+
+    res.json({
+		msg: "utilisateur mis à jour",
+		user: users[userIndex],
+	})
+
+}); 
+
+// Supprimer un utilisateur
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    // trouve son index, verifier si le userIndex est positive
+	const userIndex = users.findIndex((user) => user.id === id)
+
+	// utilisateur non trouvé
+	if (userIndex < 0)
+		return res.status(404).json({ msg: "utilisateur non trouvé" })
+    // si el est trouvé
+	users.splice(userIndex, 1)
+
+	res.json({
+		msg: "utilisateur suprimée",
+	})
+}); 
+
+module.exports = router;
