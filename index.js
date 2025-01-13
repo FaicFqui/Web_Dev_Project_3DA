76d6cc89b114f3d71a53bda354def49dc0d48441
FQUIHI Faiçal <134364426+FaicFqui@users.noreply.github.com>
2025-01-13 01:46:39 +0100

    ajout de CORS pour communiquer avec FRONT END

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors');  // Ajout de CORS
 const app = express();
 const port = 3000;
 const usersRoutes = require('./usersRoutes');
+
+app.use(cors());
+
 app.use(express.json());
 
 // Utilisation des routes des utilisateurs
 app.use('/api/users', usersRoutes);
+
+
+
 
 //démarrage du serveur
 app.listen(port, () => {
